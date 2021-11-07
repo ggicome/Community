990f6e17aae15fee947a77399e57c9fe92e5bc66
ggicome <454449493@qq.com>
2021-11-07 22:54:21 +0800

    first commit

---
+++ community/community-core/src/main/java/com/ggicome/community/Controller/BlogController.java
@@ -0,0 +1,17 @@
+package com.ggicome.community.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: community
+ * @description: blog
+ * @author: ggicome
+ * @date: 2021-11-07
+ **/
+@RestController
+@RequestMapping("/blog")
+public class BlogController {
+
+}
